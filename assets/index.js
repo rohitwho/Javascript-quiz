a5ced47243e27345b0cd7d38e4dbbe31fcfb34c9
Rohit Nayyar <Rohitnayyar54@gmail.com>
2023-03-19 00:10:58 -0400

    build  ui

---
+++ assets/index.js
@@ -0,0 +1,7 @@
+const restartBtn = document.getElementById("restart");
+const prevBtn = document.getElementById("prev");
+const submitBtn = document.getElementById("submit");
+const truetBtn = document.getElementById("true");
+const falsetBtn = document.getElementById("false");
+const questionText = document.getElementById("question-text");
+
